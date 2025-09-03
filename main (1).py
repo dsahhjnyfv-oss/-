9d1d8a8528296eb4f24c1591ed3f8bab86b70ab9
dsahhjnyfv-oss <dsahhjnyfv@gmail.com>
2025-09-03 09:11:47 +0300

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,32 @@
+years = 17
+part = 9.8
+name = 'Daryna'
+data = [3, 4, 5, 6]
+hist = (True,)
+idx = 41
+price_list = {29, 30}
+duration_time = (3, 19)
+notes = {'2': 'Storm'}
+block = b"Hello"
+
+print(type(years))
+print(type(part))
+print(type(name))
+print(type(data))
+print(type(hist))
+print(type(price_list))
+print(type(idx))
+print(type(duration_time))
+print(type(notes))
+print(type(block))
+
+print(years)
+print(part)
+print(name)
+print(data)
+print(hist)
+print(price_list)
+print(idx)
+print(duration_time)
+print(notes)
+print(block)
